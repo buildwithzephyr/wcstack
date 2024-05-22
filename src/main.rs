ea6ab88873a7dfed92eb54413e00c3edb1435d9f
Jordan Rickman <jordan.rickman.42+zephyr@gmail.com>
2024-05-21 20:55:45 -0700

    Print error message instead of the Debug text.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use clap::Parser;
+use std::process;
 use wcstack::{commands::Cli, errors::WcStackError, store::Store};
 
-fn main() -> Result<(), WcStackError> {
-    let cli = Cli::parse();
-    let store = Store::new_in_current_workspace()?;
-    cli.command.execute(store)?;
+// The type parameter T lets us call this in an unwrap_or_else. Never type (!) coerces to any type,
+// but FnOnce -> ! does not coerce to any FnOnce.
+fn exit_with_error<T>(error: impl Into<WcStackError>) -> T {
+    eprintln!("{}", error.into().to_string());
+    process::exit(1)
+}
 
-    Ok(())
+fn main() {
+    let cli = Cli::parse();
+    let store = Store::new_in_current_workspace().unwrap_or_else(exit_with_error);
+    cli.command.execute(store).unwrap_or_else(exit_with_error);
 }
